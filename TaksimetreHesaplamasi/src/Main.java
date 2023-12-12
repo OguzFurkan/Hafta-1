00de61edffc458387204c5423d6e4e0736498964
Oğuzhan Furkan Özdemir <ofozdemir14@gmail.com>
2023-12-13 02:25:02 +0300

    Taksimetre Hesaplama Programı

--- TaksimetreHesaplamasi/src/Main.java
+++ TaksimetreHesaplamasi/src/Main.java
@@ -3,8 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        double km, kmUcreti, indiBindi = 20, tutar;
 
+        System.out.println("Mesafeyi KM türü ile yazın");
+
+        Scanner mesafe = new Scanner(System.in);
+
+        km = mesafe.nextDouble();
+        kmUcreti = km * 2.20;
+        tutar = kmUcreti + 10;
+
+        if (tutar >= 20) {
+
+            System.out.println("Ödeyeceğiniz tutar " + tutar + "TL");
+
+        } else if (tutar < 20) {
+            System.out.println("Ödeyeceğiniz tutar " + indiBindi + "TL");
+        }
 
 
     }
