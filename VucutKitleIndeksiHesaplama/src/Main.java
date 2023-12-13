477fffdceba8484b2c52e937fc458b7d3d5956b7
Oğuzhan Furkan Özdemir <ofozdemir14@gmail.com>
2023-12-13 13:27:56 +0300

    Dairenin Alanını ve Çevresini Hesaplama

--- VucutKitleIndeksiHesaplama/src/Main.java
+++ VucutKitleIndeksiHesaplama/src/Main.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        // Değişkenler belirlendi.
+        double kg, boy, vki;
+
+        //Kullanıcadan vki'yi hesaplamak için veriler alındı.
+        Scanner veri = new Scanner(System.in);
+
+        //Kullanıcın girdiği veriler ile değişkenler atandı.
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz :");
+        boy = veri.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz :");
+        kg = veri.nextDouble();
+        vki = kg / (boy * boy);
+
+        System.out.println("Vucüt Kitle Indeksiniz : " + vki);
 
 
-
-        }
     }
+}
